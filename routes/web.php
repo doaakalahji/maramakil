8ff6f1ad3b29e3c54d382caeb5252ce2159a90c7
maramakil <maram88akil@gmail.com>
2023-11-03 12:02:02 +0100

    update admin routes put middleware admin in group

--- routes/web.php
+++ routes/web.php
@@ -32,25 +32,35 @@ Route::get('/contact', function () {
 })->name('contact');
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard.dashboard');
-})->middleware(['auth', 'verified' , 'admin'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard.dashboard');
+// })->middleware(['auth', 'verified' , 'admin'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth' , 'admin'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/store_product', function () {
-        return view('dashboard.add_product');
-    })->middleware(['auth', 'verified' , 'admin'])
-      ->name("add_product");
+    // Route::get('/store_product', function () {
+    //     return view('dashboard.add_product');
+    // })->middleware(['auth', 'verified' , 'admin'])
+    //   ->name("add_product");
 
 
     Route::post('/save', [ProductController::class , "store"])->name("save_product");
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth', 'verified' , 'admin'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard.dashboard');
+    })->name('dashboard');
+
+    Route::get('/store_product', function () {
+        return view('dashboard.add_product');
+    })->name("add_product");
+});
 
 
 
